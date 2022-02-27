c8367b4f0eb6541de4be984361f800928d842197
Casey DeDore <caseydedore@gmail.com>
2022-02-26 19:23:39 -0800

    (9) add tests for State constructor

---
+++ StateMachineTesting/StateConstructorTest.cs
@@ -0,0 +1,67 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StateMachineCore;
+
+namespace StateMachineTesting
+{
+    [TestClass]
+    public class StateConstructorTest
+    {
+        [TestMethod]
+        public void EmptyConstructor()
+        {
+            var state = new State();
+
+            state.Start();
+            state.Update();
+            state.End();
+
+            //success if no exception thrown
+        }
+
+        [TestMethod]
+        public void StartOnly()
+        {
+            var startFired = false;
+            var state = new State(
+                start: () => startFired = true
+            );
+
+            state.Start();
+            state.Update();
+            state.End();
+
+            Assert.IsTrue(startFired);
+        }
+
+        [TestMethod]
+        public void UpdateOnly()
+        {
+            var updateFired = false;
+            var state = new State(
+                update: () => updateFired = true
+            );
+
+            state.Start();
+            state.Update();
+            state.End();
+
+            Assert.IsTrue(updateFired);
+        }
+
+        [TestMethod]
+        public void EndOnly()
+        {
+            var endFired = false;
+            var state = new State(
+                end: () => endFired = true
+            );
+
+            state.Start();
+            state.Update();
+            state.End();
+
+            Assert.IsTrue(endFired);
+        }
+    }
+}
